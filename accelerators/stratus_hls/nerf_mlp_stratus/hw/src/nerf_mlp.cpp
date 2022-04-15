0b4dac8431987651cc5858762df607eeb28435bb
Vignesh Suresh <vv15@illinois.edu>
2022-04-14 21:08:35 -0500

    checking wrong bias value - only layer 0 enabled

--- accelerators/stratus_hls/nerf_mlp_stratus/hw/src/nerf_mlp.cpp
+++ accelerators/stratus_hls/nerf_mlp_stratus/hw/src/nerf_mlp.cpp
@@ -13,6 +13,8 @@
 // Function to manage load operations for all layers
 void nerf_mlp::load_input_dma(uint32_t len, uint32_t offset, sc_dt::sc_int<DATA_WIDTH> *plm_input)
 {
+    ESP_REPORT_INFO("offset = %d", offset);
+ 
     dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
 
     this->dma_read_ctrl.put(dma_info);
@@ -75,122 +77,122 @@ void nerf_mlp::load_input()
         uint32_t offset = 0;
 
         // Load layer 0 weights
-        uint32_t len = round_up(15360, DMA_WORD_PER_BEAT);
+        uint32_t len = round_up(LAYER_0_INPUTS*LAYER_0_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_0);
         offset += len; 
  
         // Load layer 0 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_0_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_0);
         offset += len; 
  
         // Load layer 1 weights
-        len = round_up(65536, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS*LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_1);
         offset += len;
  
         // Load layer 1 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_1);
         offset += len; 
  
         // Load layer 2 weights
-        len = round_up(65536, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS*LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_2);
         offset += len;
 
         // Load layer 2 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_2);
         offset += len; 
  
         // Load layer 3 weights
-        len = round_up(65536, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS*LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_3);
         offset += len;
 
         // Load layer 3 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_3);
         offset += len; 
  
         // Load layer 4 weights
-        len = round_up(80896, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_4_INPUTS*LAYER_4_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_4);
         offset += len;
 
         // Load layer 4 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_4_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_4);
         offset += len; 
  
         // Load layer 5 weights
-        len = round_up(65536, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS*LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_5);
         offset += len;
 
         // Load layer 5 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_5);
         offset += len; 
  
         // Load layer 6 weights
-        len = round_up(65536, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS*LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_6);
         offset += len;
 
         // Load layer 6 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_6);
         offset += len; 
  
         // Load layer 7 weights
-        len = round_up(65536, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS*LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_7);
         offset += len;
 
         // Load layer 7 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_7);
         offset += len; 
  
         // Load layer 8 weights
-        len = round_up(71680, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_8_INPUTS*LAYER_8_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_8);
         offset += len;
 
         // Load layer 8 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_8_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_8);
         offset += len; 
  
         // Load layer 9 weights
-        len = round_up(32768, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_9_INPUTS*LAYER_9_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_9);
         offset += len;
 
         // Load layer 9 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_9_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_9);
         offset += len; 
  
         // Load layer 10 weights
-        len = round_up(372, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_10_INPUTS*LAYER_10_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_wgt_10);
         offset += len;
 
         // Load layer 10 biases
-        len = round_up(256, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_10_OUTPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_bias_10);
         offset += len; 
- 
+
         // Load input position vectors
-        len = round_up(60, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_0_INPUTS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_pos);
         offset += len; 
 
         // Load input direction vectors
-        len = round_up(24, DMA_WORD_PER_BEAT);
+        len = round_up(LAYER_8_INPUTS-LAYER_N_DIMS, DMA_WORD_PER_BEAT);
         load_input_dma(len, offset, plm_dir);
         offset += len;
 
@@ -256,7 +258,7 @@ void nerf_mlp::store_output()
 
         wait();
 
-        uint32_t len = round_up(LAYER_10_OUTPUTS, DMA_WORD_PER_BEAT);
+        uint32_t len = round_up(LAYER_0_OUTPUTS, DMA_WORD_PER_BEAT);
 
         this->store_compute_handshake();
 
@@ -297,6 +299,7 @@ void nerf_mlp::compute_kernel_tile(uint16_t input_dim, uint16_t output_dim, sc_d
         HLS_UNROLL_LOOP(ON, "read_bias");
         HLS_BREAK_ARRAY_DEPENDENCY(plm_bias);
         regs_output[bias_index] = plm_bias[bias_index];
+        ESP_REPORT_INFO("design regs_output[%d] = %ld", bias_index, regs_output[bias_index]);
     }
 
     for (uint16_t col_wgt = 0; col_wgt < output_dim; col_wgt++)
@@ -398,157 +401,157 @@ void nerf_mlp::compute_kernel()
         }
     }
 
-    ////////////////////////////////
-    // Layer 1 1x256 x 256x256
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_1, plm_bias_1,
-                            regs_pong, regs_ping, regs_mul, regs_wgt);
+    // ////////////////////////////////
+    // // Layer 1 1x256 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_1, plm_bias_1,
+    //                         regs_pong, regs_ping, regs_mul, regs_wgt);
  
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_1");
-            if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_1");
+    //         if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
+    //     }
+    // }
 
-    /////////////////////////////////
-    // Layer 2 1x256 x 256x256
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_2, plm_bias_2,
-                            regs_ping, regs_pong, regs_mul, regs_wgt);
+    // /////////////////////////////////
+    // // Layer 2 1x256 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_2, plm_bias_2,
+    //                         regs_ping, regs_pong, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_2");
-            if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_2");
+    //         if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
+    //     }
+    // }
 
-    /////////////////////////////////
-    // Layer 3 1x256 x 256x256
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_3, plm_bias_3,
-                            regs_pong, regs_ping, regs_mul, regs_wgt);
+    // /////////////////////////////////
+    // // Layer 3 1x256 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_3, plm_bias_3,
+    //                         regs_pong, regs_ping, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_3");
-            if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_3");
+    //         if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
+    //     }
+    // }
     
-    /////////////////////////////////
-    // Layer 4 1x316 x 316x256
-    ///////////////////////////////// 
-    {
-        // Read from Layer 0 inputs from scratchpad
-        for (uint16_t elem_in = LAYER_N_DIMS; elem_in < LAYER_4_INPUTS; elem_in++)
-        {
-            HLS_UNROLL_LOOP(ON, "read_inputs_4");
-            HLS_BREAK_ARRAY_DEPENDENCY(plm_pos);
-            regs_ping[elem_in] = plm_pos[elem_in-LAYER_N_DIMS];
-        }
+    // /////////////////////////////////
+    // // Layer 4 1x316 x 316x256
+    // ///////////////////////////////// 
+    // {
+    //     // Read from Layer 0 inputs from scratchpad
+    //     for (uint16_t elem_in = LAYER_N_DIMS; elem_in < LAYER_4_INPUTS; elem_in++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "read_inputs_4");
+    //         HLS_BREAK_ARRAY_DEPENDENCY(plm_pos);
+    //         regs_ping[elem_in] = plm_pos[elem_in-LAYER_N_DIMS];
+    //     }
 
-        compute_kernel_tile(LAYER_4_INPUTS, LAYER_4_OUTPUTS, plm_wgt_4, plm_bias_4,
-                            regs_ping, regs_pong, regs_mul, regs_wgt);
+    //     compute_kernel_tile(LAYER_4_INPUTS, LAYER_4_OUTPUTS, plm_wgt_4, plm_bias_4,
+    //                         regs_ping, regs_pong, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_4_OUTPUTS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_4");
-            if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_4_OUTPUTS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_4");
+    //         if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
+    //     }
+    // }
     
-    /////////////////////////////////
-    // Layer 5 1x256 x 256x256
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_5, plm_bias_5,
-                            regs_pong, regs_ping, regs_mul, regs_wgt);
+    // /////////////////////////////////
+    // // Layer 5 1x256 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_5, plm_bias_5,
+    //                         regs_pong, regs_ping, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_5");
-            if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_5");
+    //         if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
+    //     }
+    // }
     
-    /////////////////////////////////
-    // Layer 6 1x256 x 256x256
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_6, plm_bias_6,
-                            regs_ping, regs_pong, regs_mul, regs_wgt);
+    // /////////////////////////////////
+    // // Layer 6 1x256 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_6, plm_bias_6,
+    //                         regs_ping, regs_pong, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_6");
-            if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_N_DIMS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_6");
+    //         if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
+    //     }
+    // }
     
-    /////////////////////////////////
-    // Layer 7 1x256 x 256x256
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_7, plm_bias_7,
-                            regs_pong, regs_ping, regs_mul, regs_wgt);
-    }
+    // /////////////////////////////////
+    // // Layer 7 1x256 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_N_DIMS, LAYER_N_DIMS, plm_wgt_7, plm_bias_7,
+    //                         regs_pong, regs_ping, regs_mul, regs_wgt);
+    // }
 
-    /////////////////////////////////
-    // Layer 8 1x280 x 256x256
-    ///////////////////////////////// 
-    {
-        // Read from Layer 0 inputs from scratchpad
-        for (uint16_t elem_in = LAYER_N_DIMS; elem_in < LAYER_8_INPUTS; elem_in++)
-        {
-            HLS_UNROLL_LOOP(ON, "read_inputs_8");
-            HLS_BREAK_ARRAY_DEPENDENCY(plm_dir);
-            regs_ping[elem_in] = plm_dir[elem_in-LAYER_N_DIMS];
-        }
-        
-        compute_kernel_tile(LAYER_8_INPUTS, LAYER_8_OUTPUTS, plm_wgt_8, plm_bias_8,
-                            regs_ping, regs_pong, regs_mul, regs_wgt);
+    // /////////////////////////////////
+    // // Layer 8 1x280 x 256x256
+    // ///////////////////////////////// 
+    // {
+    //     // Read from Layer 0 inputs from scratchpad
+    //     for (uint16_t elem_in = LAYER_N_DIMS; elem_in < LAYER_8_INPUTS; elem_in++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "read_inputs_8");
+    //         HLS_BREAK_ARRAY_DEPENDENCY(plm_dir);
+    //         regs_ping[elem_in] = plm_dir[elem_in-LAYER_N_DIMS];
+    //     }
+    //     
+    //     compute_kernel_tile(LAYER_8_INPUTS, LAYER_8_OUTPUTS, plm_wgt_8, plm_bias_8,
+    //                         regs_ping, regs_pong, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_8_OUTPUTS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_8");
-            if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_8_OUTPUTS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_8");
+    //         if (regs_pong[row_wgt] < 0) regs_pong[row_wgt] = 0;
+    //     }
+    // }
 
-    /////////////////////////////////
-    // Layer 9 1x256 x 256x128
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_9_INPUTS, LAYER_9_OUTPUTS, plm_wgt_9, plm_bias_9,
-                            regs_pong, regs_ping, regs_mul, regs_wgt);
+    // /////////////////////////////////
+    // // Layer 9 1x256 x 256x128
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_9_INPUTS, LAYER_9_OUTPUTS, plm_wgt_9, plm_bias_9,
+    //                         regs_pong, regs_ping, regs_mul, regs_wgt);
 
-        // ReLU on the output values
-        for (uint16_t row_wgt = 0; row_wgt < LAYER_9_OUTPUTS; row_wgt++)
-        {
-            HLS_UNROLL_LOOP(ON, "relu_9");
-            if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
-        }
-    }
+    //     // ReLU on the output values
+    //     for (uint16_t row_wgt = 0; row_wgt < LAYER_9_OUTPUTS; row_wgt++)
+    //     {
+    //         HLS_UNROLL_LOOP(ON, "relu_9");
+    //         if (regs_ping[row_wgt] < 0) regs_ping[row_wgt] = 0;
+    //     }
+    // }
 
-    /////////////////////////////////
-    // Layer 10 1x128 x 128x3
-    ///////////////////////////////// 
-    {
-        compute_kernel_tile(LAYER_10_INPUTS, LAYER_10_OUTPUTS, plm_wgt_10, plm_bias_10,
-                            regs_ping, regs_pong, regs_mul, regs_wgt);
-    }
+    // /////////////////////////////////
+    // // Layer 10 1x128 x 128x3
+    // ///////////////////////////////// 
+    // {
+    //     compute_kernel_tile(LAYER_10_INPUTS, LAYER_10_OUTPUTS, plm_wgt_10, plm_bias_10,
+    //                         regs_ping, regs_pong, regs_mul, regs_wgt);
+    // }
 
     // Conclude
     {
