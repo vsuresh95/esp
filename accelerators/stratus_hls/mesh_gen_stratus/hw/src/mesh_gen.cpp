3a1f85e540e400fcc7fcec86ff2c712a7766f976
Vignesh Suresh <vv15@illinois.edu>
2022-05-05 01:23:24 -0500

    basic structure of accelerator

--- accelerators/stratus_hls/mesh_gen_stratus/hw/src/mesh_gen.cpp
+++ accelerators/stratus_hls/mesh_gen_stratus/hw/src/mesh_gen.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2011-2022 Columbia University, System Level Design Group
 // SPDX-License-Identifier: Apache-2.0
 
+#define HASH_ENTRY_LEN 2 // 3 short + 1 int + 1 int ~= 4 int = 2 long
+
 #include "mesh_gen.hpp"
 #include "mesh_gen_directives.hpp"
 
@@ -45,77 +47,32 @@ void mesh_gen::load_input()
         HLS_PROTO("load-dma");
         wait();
 
-        bool ping = true;
-        uint32_t offset = 0;
+        // number of allocated hash table entries
+        uint32_t length = round_up(num_hash_table, DMA_WORD_PER_BEAT);
 
-        // Batching
-        for (uint16_t b = 0; b < 1; b++)
+        // Fetch hash entries one by one
+        for (uint16_t offset = 0; offset < length; offset += HASH_ENTRY_LEN)
         {
-            wait();
-#if (DMA_WORD_PER_BEAT == 0)
-            uint32_t length = num_hash_table;
-#else
-            uint32_t length = round_up(num_hash_table, DMA_WORD_PER_BEAT);
-#endif
-            // Chunking
-            for (int rem = length; rem > 0; rem -= PLM_IN_WORD)
+            dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, HASH_ENTRY_LEN / DMA_WORD_PER_BEAT, DMA_SIZE);
+
+            this->dma_read_ctrl.put(dma_info);
+
+            for (uint16_t j = 0; j < HASH_ENTRY_LEN; j += DMA_WORD_PER_BEAT)
             {
+                sc_dt::sc_bv<DMA_WIDTH> dataBv;
+
+                dataBv = this->dma_read_chnl.get();
                 wait();
-                // Configure DMA transaction
-                uint32_t len = rem > PLM_IN_WORD ? PLM_IN_WORD : rem;
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
-#else
-                dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
-#endif
-                offset += len;
 
-                this->dma_read_ctrl.put(dma_info);
-
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                for (uint16_t i = 0; i < len; i++)
+                // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
+                for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
                 {
-                    sc_dt::sc_bv<DATA_WIDTH> dataBv;
-
-                    for (uint16_t k = 0; k < DMA_BEAT_PER_WORD; k++)
-                    {
-                        dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH) = this->dma_read_chnl.get();
-                        wait();
-                    }
-
-                    // Write to PLM
-                    if (ping)
-                        plm_in_ping[i] = dataBv.to_int64();
-                    else
-                        plm_in_pong[i] = dataBv.to_int64();
+                    HLS_UNROLL_SIMPLE;
+                    plm_hashtable[j + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
                 }
-#else
-                for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
-                {
-                    HLS_BREAK_DEP(plm_in_ping);
-                    HLS_BREAK_DEP(plm_in_pong);
-
-                    sc_dt::sc_bv<DMA_WIDTH> dataBv;
-
-                    dataBv = this->dma_read_chnl.get();
-                    wait();
-
-                    // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
-                    for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
-                    {
-                        HLS_UNROLL_SIMPLE;
-                        if (ping)
-                            plm_in_ping[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
-                        else
-                            plm_in_pong[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
-                    }
-                }
-#endif
-                this->load_compute_handshake();
-                ping = !ping;
             }
+
+            this->load_compute_handshake();
         }
     }
 
@@ -159,87 +116,40 @@ void mesh_gen::store_output()
     // Store
     {
         HLS_PROTO("store-dma");
-        wait();
 
-        bool ping = true;
-#if (DMA_WORD_PER_BEAT == 0)
-        uint32_t store_offset = (num_hash_table) * 1;
-#else
-        uint32_t store_offset = round_up(num_hash_table, DMA_WORD_PER_BEAT) * 1;
-#endif
-        uint32_t offset = store_offset;
+        // number of allocated hash table entries
+        uint32_t length = round_up(num_hash_table, DMA_WORD_PER_BEAT);
+        uint32_t store_offset = num_hash_table * 2; // 2 d-words per hash table entry
 
-        wait();
-        // Batching
-        for (uint16_t b = 0; b < 1; b++)
+        // Store back computed triangles for hash entries read
+        for (uint16_t offset = 0; offset < length; offset += HASH_ENTRY_LEN)
         {
             wait();
-#if (DMA_WORD_PER_BEAT == 0)
-            uint32_t length = num_hash_table;
-#else
-            uint32_t length = round_up(num_hash_table, DMA_WORD_PER_BEAT);
-#endif
+
+            uint32_t len = round_up(512*5*3*3/2, DMA_WORD_PER_BEAT); // 512 voxels, 5 triangle, 3 points, 3 dim, float
+            store_offset += len;
+
             // Chunking
-            for (int rem = length; rem > 0; rem -= PLM_OUT_WORD)
+            this->store_compute_handshake();
+
+            // Configure DMA transaction
+            dma_info_t dma_info(store_offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
+
+            this->dma_write_ctrl.put(dma_info);
+
+            for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
             {
+                sc_dt::sc_bv<DMA_WIDTH> dataBv;
 
-                this->store_compute_handshake();
-
-                // Configure DMA transaction
-                uint32_t len = rem > PLM_OUT_WORD ? PLM_OUT_WORD : rem;
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
-#else
-                dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
-#endif
-                offset += len;
-
-                this->dma_write_ctrl.put(dma_info);
-
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                for (uint16_t i = 0; i < len; i++)
+                // Read from PLM
+                wait();
+                for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
                 {
-                    // Read from PLM
-                    sc_dt::sc_int<DATA_WIDTH> data;
-                    wait();
-                    if (ping)
-                        data = plm_out_ping[i];
-                    else
-                        data = plm_out_pong[i];
-                    sc_dt::sc_bv<DATA_WIDTH> dataBv(data);
-
-                    uint16_t k = 0;
-                    for (k = 0; k < DMA_BEAT_PER_WORD - 1; k++)
-                    {
-                        this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
-                        wait();
-                    }
-                    // Last beat on the bus does not require wait(), which is
-                    // placed before accessing the PLM
-                    this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
+                    HLS_UNROLL_SIMPLE;
+                    dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = plm_triangles[i + k];
                 }
-#else
-                for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
-                {
-                    sc_dt::sc_bv<DMA_WIDTH> dataBv;
-
-                    // Read from PLM
-                    wait();
-                    for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
-                    {
-                        HLS_UNROLL_SIMPLE;
-                        if (ping)
-                            dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = plm_out_ping[i + k];
-                        else
-                            dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = plm_out_pong[i + k];
-                    }
-                    this->dma_write_chnl.put(dataBv);
-                }
-#endif
-                ping = !ping;
-            }
+                this->dma_write_chnl.put(dataBv);
+            }        
         }
     }
 
@@ -282,34 +192,42 @@ void mesh_gen::compute_kernel()
 
 
     // Compute
-    bool ping = true;
     {
-        for (uint16_t b = 0; b < 1; b++)
+        // number of allocated hash table entries
+        uint32_t length = round_up(num_hash_table, DMA_WORD_PER_BEAT);
+
+        // Perform compute for all hash table entries
+        for (uint16_t offset = 0; offset < length; offset += HASH_ENTRY_LEN)
         {
-            uint32_t in_length = num_hash_table;
-            uint32_t out_length = num_hash_table;
-            int out_rem = out_length;
+            this->compute_load_handshake();
 
-            for (int in_rem = in_length; in_rem > 0; in_rem -= PLM_IN_WORD)
+            // Read voxel block
+            uint32_t len = round_up(, DMA_WORD_PER_BEAT); // size of 512 voxels
+
+            dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, length / DMA_WORD_PER_BEAT, DMA_SIZE);
+
+            this->dma_read_ctrl.put(dma_info);
+
+            for (uint16_t j = 0; j < len; j += DMA_WORD_PER_BEAT)
             {
+                sc_dt::sc_bv<DMA_WIDTH> dataBv;
 
-                uint32_t in_len  = in_rem  > PLM_IN_WORD  ? PLM_IN_WORD  : in_rem;
-                uint32_t out_len = out_rem > PLM_OUT_WORD ? PLM_OUT_WORD : out_rem;
+                dataBv = this->dma_read_chnl.get();
+                wait();
 
-                this->compute_load_handshake();
-
-                // Computing phase implementation
-                for (int i = 0; i < in_len; i++) {
-                    if (ping)
-                        plm_out_ping[i] = plm_in_ping[i];
-                    else
-                        plm_out_pong[i] = plm_in_pong[i];
+                // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
+                for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
+                {
+                    HLS_UNROLL_SIMPLE;
+                    plm_voxelblock[j + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
                 }
-
-                out_rem -= PLM_OUT_WORD;
-                this->compute_store_handshake();
-                ping = !ping;
             }
+
+            // Calculate cube index
+
+            // Triangle calculation
+
+            this->compute_store_handshake();
         }
 
         // Conclude
