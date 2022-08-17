59d76b69aed1f8871bca4f7d57d64e0e806307cf
Vignesh Suresh <vv15@illinois.edu>
2022-08-17 14:37:01 -0500

    removed chunking code - temporary, to simplify

--- accelerators/stratus_hls/fft2_stratus/hw/src/fft2.cpp
+++ accelerators/stratus_hls/fft2_stratus/hw/src/fft2.cpp
@@ -46,7 +46,6 @@ void fft2::load_input()
     }
 
     // Load
-    int loads_done = 0;
     {
         HLS_PROTO("load-dma");
         uint32_t offset = 0;
@@ -57,59 +56,52 @@ void fft2::load_input()
 #else
         uint32_t length = round_up(2 * num_ffts * num_samples, DMA_WORD_PER_BEAT);
 #endif
-        // Chunking
-        for (int rem = length; rem > 0; rem -= PLM_IN_WORD)
+        // Configure DMA transaction
+        uint32_t len = length;
+#if (DMA_WORD_PER_BEAT == 0)
+        // data word is wider than NoC links
+        dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
+#else
+        dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
+#endif
+        offset += len;
+
+        this->dma_read_ctrl.put(dma_info);
+
+#if (DMA_WORD_PER_BEAT == 0)
+        // data word is wider than NoC links
+        for (uint16_t i = 0; i < len; i++)
         {
-            wait();
-            // Configure DMA transaction
-            uint32_t len = rem > PLM_IN_WORD ? PLM_IN_WORD : rem;
-#if (DMA_WORD_PER_BEAT == 0)
-            // data word is wider than NoC links
-            dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
-#else
-            dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
-#endif
-            offset += len;
+            sc_dt::sc_bv<DATA_WIDTH> dataBv;
 
-            this->dma_read_ctrl.put(dma_info);
-
-#if (DMA_WORD_PER_BEAT == 0)
-            // data word is wider than NoC links
-            for (uint16_t i = 0; i < len; i++)
+            for (uint16_t k = 0; k < DMA_BEAT_PER_WORD; k++)
             {
-                sc_dt::sc_bv<DATA_WIDTH> dataBv;
-
-                for (uint16_t k = 0; k < DMA_BEAT_PER_WORD; k++)
-                {
-                    dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH) = this->dma_read_chnl.get();
-                    wait();
-                }
-
-                // Write to PLM
-                A0[i] = dataBv.to_int64();
-            }
-#else
-            for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
-            {
-                HLS_BREAK_DEP(A0);
-
-                sc_dt::sc_bv<DMA_WIDTH> dataBv;
-
-                dataBv = this->dma_read_chnl.get();
+                dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH) = this->dma_read_chnl.get();
                 wait();
-
-                // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
-                for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
-                {
-                    HLS_UNROLL_SIMPLE;
-                    A0[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
-                }
             }
+
+            // Write to PLM
+            A0[i] = dataBv.to_int64();
+        }
+#else
+        for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
+        {
+            HLS_BREAK_DEP(A0);
+
+            sc_dt::sc_bv<DMA_WIDTH> dataBv;
+
+            dataBv = this->dma_read_chnl.get();
+            wait();
+
+            // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
+            for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
+            {
+                HLS_UNROLL_SIMPLE;
+                A0[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
+            }
+        }
 #endif
-            this->load_compute_handshake();
-            this->load_to_store_handshake();
-            loads_done++;
-        } // for (rem = length downto 0 )
+        this->load_compute_handshake();
     } // Load scope
 
     // Conclude
@@ -155,7 +147,6 @@ void fft2::store_output()
     }
 
     // Store
-    int stores_done = 0;
     {
         HLS_PROTO("store-dma");
         wait();
@@ -167,68 +158,63 @@ void fft2::store_output()
 #endif
         uint32_t offset = 0;
 
-        wait();
 #if (DMA_WORD_PER_BEAT == 0)
         uint32_t length = 2 * num_ffts * num_samples;
 #else
         uint32_t length = round_up(2 * num_ffts * num_samples, DMA_WORD_PER_BEAT);
 #endif
-        // Chunking
-        for (int rem = length; rem > 0; rem -= PLM_OUT_WORD)
+        this->store_compute_handshake();
+
+        // Configure DMA transaction
+        uint32_t len = length;
+#if (DMA_WORD_PER_BEAT == 0)
+        // data word is wider than NoC links
+        dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
+#else
+        dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
+#endif
+        offset += len;
+
+        this->dma_write_ctrl.put(dma_info);
+
+#if (DMA_WORD_PER_BEAT == 0)
+        // data word is wider than NoC links
+        for (uint16_t i = 0; i < len; i++)
         {
-            this->store_compute_handshake();
+            // Read from PLM
+            sc_dt::sc_int<DATA_WIDTH> data;
+            wait();
+            data = A0[i];
+            sc_dt::sc_bv<DATA_WIDTH> dataBv(data);
 
-            // Configure DMA transaction
-            uint32_t len = rem > PLM_OUT_WORD ? PLM_OUT_WORD : rem;
-#if (DMA_WORD_PER_BEAT == 0)
-            // data word is wider than NoC links
-            dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
-#else
-            dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
-#endif
-            offset += len;
-
-            this->dma_write_ctrl.put(dma_info);
-
-#if (DMA_WORD_PER_BEAT == 0)
-            // data word is wider than NoC links
-            for (uint16_t i = 0; i < len; i++)
+            uint16_t k = 0;
+            for (k = 0; k < DMA_BEAT_PER_WORD - 1; k++)
             {
-                // Read from PLM
-                sc_dt::sc_int<DATA_WIDTH> data;
-                wait();
-                data = A0[i];
-                sc_dt::sc_bv<DATA_WIDTH> dataBv(data);
-
-                uint16_t k = 0;
-                for (k = 0; k < DMA_BEAT_PER_WORD - 1; k++)
-                {
-                    this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
-                    wait();
-                }
-                // Last beat on the bus does not require wait(), which is
-                // placed before accessing the PLM
                 this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
-            }
-#else
-            for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
-            {
-                sc_dt::sc_bv<DMA_WIDTH> dataBv;
-
-                // Read from PLM
                 wait();
-                for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
-                {
-                    HLS_UNROLL_SIMPLE;
-                    dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = A0[i + k];
-                }
-                this->dma_write_chnl.put(dataBv);
             }
+            // Last beat on the bus does not require wait(), which is
+            // placed before accessing the PLM
+            this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
+        }
+#else
+        for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
+        {
+            sc_dt::sc_bv<DMA_WIDTH> dataBv;
+
+            // Read from PLM
+            wait();
+            for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
+            {
+                HLS_UNROLL_SIMPLE;
+                dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = A0[i + k];
+            }
+            this->dma_write_chnl.put(dataBv);
+        }
 #endif
-            stores_done++;
-            this->store_to_load_handshake();
-        } // for (rem = length downto 0
+        this->store_to_load_handshake();
     } // Store scope
+
     // Conclude
     {
         this->accelerator_done();
@@ -280,96 +266,80 @@ void fft2::compute_kernel()
     // Compute
     // Loop through the num_ffts successive FFT computations
     {
-        uint32_t in_length  = 2 * num_ffts * num_samples;
-        uint32_t out_length = in_length;
-        uint32_t out_rem = out_length;
-        unsigned max_in_ffts = 1 << (MAX_LOGN_SAMPLES - logn_samples);
-        unsigned ffts_done = 0;
-        // Chunking : Load/Store Memory transfers (refill memory)
-        for (int in_rem = in_length; in_rem > 0; in_rem -= PLM_IN_WORD)
-        {
-            uint32_t in_len  = in_rem  > PLM_IN_WORD  ? PLM_IN_WORD  : in_rem;
-            uint32_t out_len = out_rem > PLM_OUT_WORD ? PLM_OUT_WORD : out_rem;
+        // Compute FFT(s) in the PLM
+        this->compute_load_handshake();
 
-            // Compute FFT(s) in the PLM
-            this->compute_load_handshake();
-            unsigned rem_ffts = (num_ffts - ffts_done); 
-            unsigned in_ffts =  (rem_ffts > max_in_ffts) ? max_in_ffts : rem_ffts;
-            for (unsigned fftn = 0; fftn < in_ffts; fftn++) {
-                unsigned offset = fftn * num_samples;  // Offset into Mem for start of this FFT
-                int sin_sign = (do_inverse) ? -1 : 1; // This modifes the mySin
-                                                      // values used below
-                if (do_inverse && do_shift) {
-                    fft2_do_shift(offset, num_samples, logn_samples);
-                }
-
-                // Do the bit-reverse
-                fft2_bit_reverse(offset, num_samples, logn_samples);
-
-                // Computing phase implementation
-                int m = 1;  // iterative FFT
-
-            FFT2_SINGLE_L1:
-                for(unsigned s = 1; s <= logn_samples; s++) {
-                    m = 1 << s;
-                    CompNum wm(myCos(s), sin_sign*mySin(s));
-
-                FFT2_SINGLE_L2:
-                    for(unsigned k = 0; k < num_samples; k +=m) {
-
-                        CompNum w((FPDATA) 1, (FPDATA) 0);
-                        int md2 = m / 2;
-
-                    FFT2_SINGLE_L3:
-                        for(int j = 0; j < md2; j++) {
-
-                            int kj = offset + k + j;
-                            int kjm = offset + k + j + md2;
-                            CompNum akj, akjm;
-                            CompNum bkj, bkjm;
-
-                            akj.re = int2fp<FPDATA, WORD_SIZE>(A0[2 * kj]);
-                            akj.im = int2fp<FPDATA, WORD_SIZE>(A0[2 * kj + 1]);
-                            akjm.re = int2fp<FPDATA, WORD_SIZE>(A0[2 * kjm]);
-                            akjm.im = int2fp<FPDATA, WORD_SIZE>(A0[2 * kjm + 1]);
-
-                            CompNum t;
-                            compMul(w, akjm, t);
-                            CompNum u(akj.re, akj.im);
-                            compAdd(u, t, bkj);
-                            compSub(u, t, bkjm);
-                            CompNum wwm;
-                            wwm.re = w.re - (wm.im * w.im + wm.re * w.re);
-                            wwm.im = w.im + (wm.im * w.re - wm.re * w.im);
-                            w = wwm;
-
-                            {
-                                HLS_PROTO("compute_write_A0");
-                                HLS_BREAK_DEP(A0);
-                                wait();
-                                A0[2 * kj] = fp2int<FPDATA, WORD_SIZE>(bkj.re);
-                                A0[2 * kj + 1] = fp2int<FPDATA, WORD_SIZE>(bkj.im);
-                                wait();
-                                A0[2 * kjm] = fp2int<FPDATA, WORD_SIZE>(bkjm.re);
-                                A0[2 * kjm + 1] = fp2int<FPDATA, WORD_SIZE>(bkjm.im);
-                            }
-                        } // for (j = 0 .. md2)
-                    } // for (k = 0 .. num_samples)
-                } // for (s = 1 .. logn_samples)
-
-                if ((!do_inverse) && (do_shift)) {
-                    fft2_do_shift(offset, num_samples, logn_samples);
-                }
-                ffts_done++;
-                offset += num_samples;  // Offset into Mem for start of this FFT
-            } // for( n = 0 .. mnum_ffts)
-            out_rem -= PLM_OUT_WORD;
-            this->compute_store_handshake();
-        } // for (in_rem = in_length downto 0)
-
-        // Conclude
-        {
-            this->process_done();
+        unsigned offset = 0;  // Offset into Mem for start of this FFT
+        int sin_sign = (do_inverse) ? -1 : 1; // This modifes the mySin
+                                              // values used below
+        if (do_inverse && do_shift) {
+            fft2_do_shift(offset, num_samples, logn_samples);
         }
+
+        // Do the bit-reverse
+        fft2_bit_reverse(offset, num_samples, logn_samples);
+
+        // Computing phase implementation
+        int m = 1;  // iterative FFT
+
+        FFT2_SINGLE_L1:
+            for(unsigned s = 1; s <= logn_samples; s++) {
+                m = 1 << s;
+                CompNum wm(myCos(s), sin_sign*mySin(s));
+
+            FFT2_SINGLE_L2:
+                for(unsigned k = 0; k < num_samples; k +=m) {
+
+                    CompNum w((FPDATA) 1, (FPDATA) 0);
+                    int md2 = m / 2;
+
+                FFT2_SINGLE_L3:
+                    for(int j = 0; j < md2; j++) {
+
+                        int kj = offset + k + j;
+                        int kjm = offset + k + j + md2;
+                        CompNum akj, akjm;
+                        CompNum bkj, bkjm;
+
+                        akj.re = int2fp<FPDATA, WORD_SIZE>(A0[2 * kj]);
+                        akj.im = int2fp<FPDATA, WORD_SIZE>(A0[2 * kj + 1]);
+                        akjm.re = int2fp<FPDATA, WORD_SIZE>(A0[2 * kjm]);
+                        akjm.im = int2fp<FPDATA, WORD_SIZE>(A0[2 * kjm + 1]);
+
+                        CompNum t;
+                        compMul(w, akjm, t);
+                        CompNum u(akj.re, akj.im);
+                        compAdd(u, t, bkj);
+                        compSub(u, t, bkjm);
+                        CompNum wwm;
+                        wwm.re = w.re - (wm.im * w.im + wm.re * w.re);
+                        wwm.im = w.im + (wm.im * w.re - wm.re * w.im);
+                        w = wwm;
+
+                        {
+                            HLS_PROTO("compute_write_A0");
+                            HLS_BREAK_DEP(A0);
+                            wait();
+                            A0[2 * kj] = fp2int<FPDATA, WORD_SIZE>(bkj.re);
+                            A0[2 * kj + 1] = fp2int<FPDATA, WORD_SIZE>(bkj.im);
+                            wait();
+                            A0[2 * kjm] = fp2int<FPDATA, WORD_SIZE>(bkjm.re);
+                            A0[2 * kjm + 1] = fp2int<FPDATA, WORD_SIZE>(bkjm.im);
+                        }
+                    } // for (j = 0 .. md2)
+                } // for (k = 0 .. num_samples)
+            } // for (s = 1 .. logn_samples)
+
+        if ((!do_inverse) && (do_shift)) {
+            fft2_do_shift(offset, num_samples, logn_samples);
+        }
+
+        this->compute_store_handshake();
     } // Compute scope
+
+    // Conclude
+    {
+        this->process_done();
+    }
+
 } // Function : compute_kernel
