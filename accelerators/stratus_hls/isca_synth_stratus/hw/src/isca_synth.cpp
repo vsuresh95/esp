f9694591dc00d99299977a22b487815743f169d2
Vignesh Suresh <vv15@illinois.edu>
2022-11-13 20:18:20 -0600

    removed unwanted code in isca synth

--- accelerators/stratus_hls/isca_synth_stratus/hw/src/isca_synth.cpp
+++ accelerators/stratus_hls/isca_synth_stratus/hw/src/isca_synth.cpp
@@ -12,7 +12,6 @@
 
 void isca_synth::load_input()
 {
-
     // Reset
     {
         HLS_PROTO("load-reset");
@@ -47,78 +46,37 @@ void isca_synth::load_input()
         HLS_PROTO("load-dma");
         wait();
 
-        bool ping = true;
         uint32_t offset = 0;
 
-        // Batching
-        for (uint16_t b = 0; b < 1; b++)
+        wait();
+
+        // Configure DMA transaction
+        uint32_t len = round_up(size, DMA_WORD_PER_BEAT);
+
+        dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
+
+        offset += len;
+
+        this->dma_read_ctrl.put(dma_info);
+
+        for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
         {
+            HLS_BREAK_DEP(plm_in_ping);
+
+            sc_dt::sc_bv<DMA_WIDTH> dataBv;
+
+            dataBv = this->dma_read_chnl.get();
             wait();
-#if (DMA_WORD_PER_BEAT == 0)
-            uint32_t length = size;
-#else
-            uint32_t length = round_up(size, DMA_WORD_PER_BEAT);
-#endif
-            // Chunking
-            for (int rem = length; rem > 0; rem -= PLM_IN_WORD)
+
+            // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
+            for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
             {
-                wait();
-                // Configure DMA transaction
-                uint32_t len = rem > PLM_IN_WORD ? PLM_IN_WORD : rem;
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
-#else
-                dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
-#endif
-                offset += len;
-
-                this->dma_read_ctrl.put(dma_info);
-
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                for (uint16_t i = 0; i < len; i++)
-                {
-                    sc_dt::sc_bv<DATA_WIDTH> dataBv;
-
-                    for (uint16_t k = 0; k < DMA_BEAT_PER_WORD; k++)
-                    {
-                        dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH) = this->dma_read_chnl.get();
-                        wait();
-                    }
-
-                    // Write to PLM
-                    if (ping)
-                        plm_in_ping[i] = dataBv.to_int64();
-                    else
-                        plm_in_pong[i] = dataBv.to_int64();
-                }
-#else
-                for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
-                {
-                    HLS_BREAK_DEP(plm_in_ping);
-                    HLS_BREAK_DEP(plm_in_pong);
-
-                    sc_dt::sc_bv<DMA_WIDTH> dataBv;
-
-                    dataBv = this->dma_read_chnl.get();
-                    wait();
-
-                    // Write to PLM (all DMA_WORD_PER_BEAT words in one cycle)
-                    for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
-                    {
-                        HLS_UNROLL_SIMPLE;
-                        if (ping)
-                            plm_in_ping[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
-                        else
-                            plm_in_pong[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
-                    }
-                }
-#endif
-                this->load_compute_handshake();
-                ping = !ping;
+                HLS_UNROLL_SIMPLE;
+                plm_in_ping[i + k] = dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH).to_int64();
             }
         }
+
+        this->load_compute_handshake();
     }
 
     // Conclude
@@ -165,85 +123,32 @@ void isca_synth::store_output()
         HLS_PROTO("store-dma");
         wait();
 
-        bool ping = true;
-#if (DMA_WORD_PER_BEAT == 0)
-        uint32_t store_offset = (size) * 1;
-#else
-        uint32_t store_offset = round_up(size, DMA_WORD_PER_BEAT) * 1;
-#endif
-        uint32_t offset = store_offset;
+        uint32_t offset = round_up(size, DMA_WORD_PER_BEAT) * 1;
+        uint32_t len = round_up(size, DMA_WORD_PER_BEAT);
 
         wait();
-        // Batching
-        for (uint16_t b = 0; b < 1; b++)
+
+        this->store_compute_handshake();
+
+        // Configure DMA transaction
+        dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
+        offset += len;
+
+        this->dma_write_ctrl.put(dma_info);
+
+        for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
         {
+            sc_dt::sc_bv<DMA_WIDTH> dataBv;
+
+            // Read from PLM
             wait();
-#if (DMA_WORD_PER_BEAT == 0)
-            uint32_t length = size;
-#else
-            uint32_t length = round_up(size, DMA_WORD_PER_BEAT);
-#endif
-            // Chunking
-            for (int rem = length; rem > 0; rem -= PLM_OUT_WORD)
+            for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
             {
-
-                this->store_compute_handshake();
-
-                // Configure DMA transaction
-                uint32_t len = rem > PLM_OUT_WORD ? PLM_OUT_WORD : rem;
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
-#else
-                dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
-#endif
-                offset += len;
-
-                this->dma_write_ctrl.put(dma_info);
-
-#if (DMA_WORD_PER_BEAT == 0)
-                // data word is wider than NoC links
-                for (uint16_t i = 0; i < len; i++)
-                {
-                    // Read from PLM
-                    sc_dt::sc_int<DATA_WIDTH> data;
-                    wait();
-                    if (ping)
-                        data = plm_out_ping[i];
-                    else
-                        data = plm_out_pong[i];
-                    sc_dt::sc_bv<DATA_WIDTH> dataBv(data);
-
-                    uint16_t k = 0;
-                    for (k = 0; k < DMA_BEAT_PER_WORD - 1; k++)
-                    {
-                        this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
-                        wait();
-                    }
-                    // Last beat on the bus does not require wait(), which is
-                    // placed before accessing the PLM
-                    this->dma_write_chnl.put(dataBv.range((k+1) * DMA_WIDTH - 1, k * DMA_WIDTH));
-                }
-#else
-                for (uint16_t i = 0; i < len; i += DMA_WORD_PER_BEAT)
-                {
-                    sc_dt::sc_bv<DMA_WIDTH> dataBv;
-
-                    // Read from PLM
-                    wait();
-                    for (uint16_t k = 0; k < DMA_WORD_PER_BEAT; k++)
-                    {
-                        HLS_UNROLL_SIMPLE;
-                        if (ping)
-                            dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = plm_out_ping[i + k];
-                        else
-                            dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = plm_out_pong[i + k];
-                    }
-                    this->dma_write_chnl.put(dataBv);
-                }
-#endif
-                ping = !ping;
+                HLS_UNROLL_SIMPLE;
+                dataBv.range((k+1) * DATA_WIDTH - 1, k * DATA_WIDTH) = plm_out_ping[i + k];
             }
+
+            this->dma_write_chnl.put(dataBv);
         }
     }
 
@@ -286,41 +191,18 @@ void isca_synth::compute_kernel()
         size = config.size;
     }
 
-
     // Compute
-    bool ping = true;
+    for (uint32_t i = 0; i < compute_ratio; i++)
+	{
+        HLS_UNROLL_LOOP(OFF)
+        {
+            HLS_DEFINE_PROTOCOL("compute-delay");
+            wait();
+	    }
+    }
+
+    // Conclude
     {
-        for (uint16_t b = 0; b < 1; b++)
-        {
-            uint32_t in_length = size;
-            uint32_t out_length = size;
-            int out_rem = out_length;
-
-            for (int in_rem = in_length; in_rem > 0; in_rem -= PLM_IN_WORD)
-            {
-
-                uint32_t in_len  = in_rem  > PLM_IN_WORD  ? PLM_IN_WORD  : in_rem;
-                uint32_t out_len = out_rem > PLM_OUT_WORD ? PLM_OUT_WORD : out_rem;
-
-                this->compute_load_handshake();
-
-                // Computing phase implementation
-                for (int i = 0; i < in_len; i++) {
-                    if (ping)
-                        plm_out_ping[i] = plm_in_ping[i];
-                    else
-                        plm_out_pong[i] = plm_in_pong[i];
-                }
-
-                out_rem -= PLM_OUT_WORD;
-                this->compute_store_handshake();
-                ping = !ping;
-            }
-        }
-
-        // Conclude
-        {
-            this->process_done();
-        }
+        this->process_done();
     }
 }
