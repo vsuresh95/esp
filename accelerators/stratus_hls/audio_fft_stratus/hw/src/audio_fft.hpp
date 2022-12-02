97a203e318ed9fec73c0b0426a96c839efb9265b
Vignesh Suresh <vv15@illinois.edu>
2022-12-02 16:36:07 -0600

    Created FFT and FIR acc from script for audio

---
+++ accelerators/stratus_hls/audio_fft_stratus/hw/src/audio_fft.hpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2011-2022 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef __AUDIO_FFT_HPP__
+#define __AUDIO_FFT_HPP__
+
+#include "audio_fft_conf_info.hpp"
+#include "audio_fft_debug_info.hpp"
+
+#include "esp_templates.hpp"
+
+#include "audio_fft_directives.hpp"
+
+#define __round_mask(x, y) ((y)-1)
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+/* <<--defines-->> */
+#define DATA_WIDTH 32
+#define DMA_SIZE SIZE_WORD
+#define PLM_OUT_WORD 2
+#define PLM_IN_WORD 2
+
+class audio_fft : public esp_accelerator_3P<DMA_WIDTH>
+{
+public:
+    // Constructor
+    SC_HAS_PROCESS(audio_fft);
+    audio_fft(const sc_module_name& name)
+    : esp_accelerator_3P<DMA_WIDTH>(name)
+        , cfg("config")
+    {
+        // Signal binding
+        cfg.bind_with(*this);
+
+        // Map arrays to memories
+        /* <<--plm-bind-->> */
+        HLS_MAP_plm(plm_out_pong, PLM_OUT_NAME);
+        HLS_MAP_plm(plm_out_ping, PLM_OUT_NAME);
+        HLS_MAP_plm(plm_in_pong, PLM_IN_NAME);
+        HLS_MAP_plm(plm_in_ping, PLM_IN_NAME);
+    }
+
+    // Processes
+
+    // Load the input data
+    void load_input();
+
+    // Computation
+    void compute_kernel();
+
+    // Store the output data
+    void store_output();
+
+    // Configure audio_fft
+    esp_config_proc cfg;
+
+    // Functions
+
+    // Private local memories
+    sc_dt::sc_int<DATA_WIDTH> plm_in_ping[PLM_IN_WORD];
+    sc_dt::sc_int<DATA_WIDTH> plm_in_pong[PLM_IN_WORD];
+    sc_dt::sc_int<DATA_WIDTH> plm_out_ping[PLM_OUT_WORD];
+    sc_dt::sc_int<DATA_WIDTH> plm_out_pong[PLM_OUT_WORD];
+
+};
+
+
+#endif /* __AUDIO_FFT_HPP__ */
---
+++ accelerators/stratus_hls/audio_fft_stratus/hw/src/audio_fft_functions.hpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2011-2022 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#include "audio_fft.hpp"
+
+// Optional application-specific helper functions
