1172da9ea79670cbfb8df2f5844f50d632aba593
vv15 <vv15@illinois.edu>
2023-09-08 20:12:50 -0500

    Added multicore baremetal app

---
+++ soft/common/apps/baremetal/multicore_test/README.md
@@ -0,0 +1,11 @@
+# Bare metal app for Multicore test
+
+## Default build steps
+```
+make multicore_test-baremetal
+```
+
+## Clean
+```
+make multicore_test-baremetal-clean
+```
---
+++ soft/common/apps/baremetal/multicore_test/multicore_test.c
@@ -0,0 +1,12 @@
+/* Copyright (c) 2011-2019 Columbia University, System Level Design Group */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <stdio.h>
+#ifndef __riscv
+#include <stdlib.h>
+#endif
+
+int main(int argc, char * argv[])
+{
+	return 0;
+}
