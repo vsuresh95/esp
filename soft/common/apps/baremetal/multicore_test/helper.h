57fa311161456779e5438b4ae8be936b97ca4b26
vv15 <vv15@illinois.edu>
2023-09-09 16:25:29 -0500

    Working multicore test for 2,4 core

---
+++ soft/common/apps/baremetal/multicore_test/helper.h
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+void thread_entry (int cid, int nc) {
+    return;
+}
+
+unsigned amo_add (volatile unsigned *handshake, unsigned value) {
+	unsigned old_val;
+
+	asm volatile (
+		"mv t0, %2;"
+		"mv t2, %1;"
+ 		"amoadd.w.aqrl t1, t0, (t2);"
+		"mv %0, t1"
+		: "=r" (old_val)
+		: "r" (handshake), "r" (value)
+		: "t0", "t1", "t2", "memory"
+		);
+
+	return old_val;
+}
+
+unsigned amo_swap (volatile unsigned *handshake, unsigned value) {
+	unsigned old_val;
+
+	asm volatile (
+		"mv t0, %2;"
+		"mv t2, %1;"
+ 		"amoswap.w.aqrl t1, t0, (t2);"
+		"mv %0, t1"
+		: "=r" (old_val)
+		: "r" (handshake), "r" (value)
+		: "t0", "t1", "t2", "memory"
+		);
+
+	return old_val;
+}
+
+void acquire_lock (volatile unsigned *handshake) {
+    unsigned old_val;
+
+    // acquire the lock
+    while (1) {
+        // check if lock is set
+        if (*handshake != 1) {
+            // try to set lock
+            old_val = amo_swap (handshake, 1); 
+
+            // check if lock was set
+            if (old_val != 1){
+                break;
+            }
+        }
+    }
+}
+
+void release_lock (volatile unsigned *handshake) {
+    *handshake = 0;
+    asm volatile ("fence w, w");
+}
+
+void multicore_print(const char *fmt, ...) {
+    // Lock to acquire before printing
+	volatile unsigned* print_lock = (volatile unsigned*) 0x90010000;
+	
+    // acquire the lock
+    acquire_lock (print_lock);
+
+    printf(fmt);
+
+    // release the lock
+    release_lock (print_lock);
+}
--- soft/common/apps/baremetal/multicore_test/multicore_test.c
+++ soft/common/apps/baremetal/multicore_test/multicore_test.c
@@ -1,12 +1,86 @@
 /* Copyright (c) 2011-2019 Columbia University, System Level Design Group */
 /* SPDX-License-Identifier: Apache-2.0 */
 
-#include <stdio.h>
-#ifndef __riscv
-#include <stdlib.h>
-#endif
+#include <helper.h>
 
 int main(int argc, char * argv[])
 {
+ 	unsigned hartid;
+
+ 	asm volatile (
+ 		"csrr %0, mhartid"
+ 		: "=r" (hartid)
+ 	);
+	
+	volatile unsigned* lock = (volatile unsigned*) 0x90010020;
+	volatile unsigned* checkpoint = (volatile unsigned*) 0x90010030;
+	volatile unsigned* first = (volatile unsigned*) 0x90010040;
+
+	volatile unsigned* buffer1 = (volatile unsigned*) 0x90020000;
+	const unsigned buffer1_size = 1024 * NUM_THREADS;
+
+	unsigned errors = 0;
+
+	multicore_print("[HART %d] Hello from ESP!\n", hartid);
+
+	if (amo_swap(first, 1) == 1) {
+		amo_add(checkpoint, 1);
+	} else {
+		*checkpoint = 1;
+	}
+
+	while(*checkpoint != NUM_THREADS);
+	*checkpoint = 0;
+
+	// CP 1 : Each core write to alternating elements of an array.
+	errors = 0;
+
+	for (unsigned i = hartid; i < buffer1_size; i+=NUM_THREADS) {
+		buffer1[i] = (hartid + 1) * i;
+	}
+
+	amo_add(checkpoint, 1);
+	while(*checkpoint != NUM_THREADS);
+	*checkpoint = 0;
+
+	// CP 2 : All cores will test the values of the array
+	for (unsigned i = ((hartid + 1) % NUM_THREADS); i < buffer1_size; i+=NUM_THREADS) {
+		if (buffer1[i] != ((i % NUM_THREADS + 1) * i)) {
+			printf("[HART %d] %d E = %d A = %d\n", hartid, i, ((i % NUM_THREADS + 1) * i), buffer1[i]);
+			errors++;
+		}
+	}
+	
+	multicore_print("[HART %d] Errors = %d\n", hartid, errors);
+
+	amo_add(checkpoint, 1);
+	while(*checkpoint != NUM_THREADS);
+	*checkpoint = 0;
+
+	// CP 3 : Each core amo_add a value to alternating elements of an array.
+	for (unsigned i = hartid; i < buffer1_size; i+=NUM_THREADS) {
+		amo_add(buffer1+i, (hartid + 1) * i);
+	}
+
+	amo_add(checkpoint, 1);
+	while(*checkpoint != NUM_THREADS);
+	*checkpoint = 0;
+
+	// CP 4 : All cores will test the values of the array
+	errors = 0;
+
+	for (unsigned i = ((hartid + 1) % NUM_THREADS); i < buffer1_size; i+=NUM_THREADS) {
+		if (buffer1[i] != ((i % NUM_THREADS + 1) * 2 * i)) {
+			printf("[HART %d] %d E = %d A = %d\n", hartid, i, ((i % NUM_THREADS + 1) * i), buffer1[i]);
+ 			errors++;
+		}
+	}
+
+	multicore_print("[HART %d] Errors = %d\n", hartid, errors);
+
+	amo_add(checkpoint, 1);
+	while(*checkpoint != NUM_THREADS);
+	*checkpoint = 0;
+
 	return 0;
 }
