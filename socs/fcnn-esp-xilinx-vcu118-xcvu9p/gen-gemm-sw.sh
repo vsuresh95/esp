ecc58b66f870df3a4bf3f6153e6614c05e5c1916
vv15 <vv15@illinois.edu>
2024-07-25 00:42:49 -0500

    Added scripts for GEMM FCNN gen and run

--- socs/fcnn-esp-xilinx-vcu118-xcvu9p/gen-gemm-sw.sh
+++ socs/fcnn-esp-xilinx-vcu118-xcvu9p/gen-gemm-sw.sh
@@ -10,44 +10,52 @@ then
     mkdir test/gemm-bm/
 fi
 
-# OS
-if [ ! -d test/gemm-os/ ]
+# Linux
+if [ ! -d test/gemm/ ]
 then
-    mkdir test/gemm-os/
-fi
-
-# MESI
-if [ ! -d test/gemm-mesi/ ]
-then
-    mkdir test/gemm-mesi/
-fi
-
-# DMA
-if [ ! -d test/gemm-dma/ ]
-then
-    mkdir test/gemm-dma/
+    mkdir test/gemm/
 fi
 
 # GEMM
-# TODO
-for length in 32 64 128 256 512 1024
-do
-    # Bare metal
-    GEMM_LEN=$length ENABLE_SM=0 IS_ESP=1 COH_MODE=0 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
-    cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-${length}.bin
-    cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-${length}.exe
+# Bare metal
+D3=64 D2=64 D1=1 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-1.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-1.exe
 
-    # OS
-    GEMM_LEN=$length ENABLE_SM=0 IS_ESP=1 COH_MODE=0 make gemm_stratus-app-clean gemm_stratus-app
-    cp soft-build/ariane/sysroot/applications/test/gemm_stratus.exe test/gemm-os/gemm-os-${length}.exe
+D3=8 D2=8 D1=8 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-2.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-2.exe
 
-    # MESI
-    GEMM_LEN=$length ENABLE_SM=1 IS_ESP=1 COH_MODE=0 make gemm_stratus-app-clean gemm_stratus-app
-    cp soft-build/ariane/sysroot/applications/test/gemm_stratus.exe test/gemm-mesi/gemm-mesi-${length}.exe
+D3=16 D2=16 D1=16 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-3.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-3.exe
 
-    # DMA
-    GEMM_LEN=$length ENABLE_SM=1 IS_ESP=1 COH_MODE=1 make gemm_stratus-app-clean gemm_stratus-app
-    cp soft-build/ariane/sysroot/applications/test/gemm_stratus.exe test/gemm-dma/gemm-dma-${length}.exe
+D3=16 D2=32 D1=16 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-4.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-4.exe
 
-done
+D3=32 D2=32 D1=32 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-5.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-5.exe
+
+D3=32 D2=64 D1=32 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-6.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-6.exe
+
+D3=64 D2=64 D1=64 make gemm_stratus-baremetal-clean gemm_stratus-baremetal
+cp soft-build/ariane/baremetal/gemm_stratus.bin test/gemm-bm/gemm-bm-7.bin
+cp soft-build/ariane/baremetal/gemm_stratus.exe test/gemm-bm/gemm-bm-7.exe
+
+# OS
+COMP_MODE=0 make gemm_stratus-app-clean gemm_stratus-app
+cp soft-build/ariane/sysroot/applications/test/gemm_stratus.exe test/gemm/gemm-os.exe
+
+# Chaining
+# MESI
+ENABLE_SM=1 COMP_MODE=1 make gemm_stratus-app-clean gemm_stratus-app
+cp soft-build/ariane/sysroot/applications/test/gemm_stratus.exe test/gemm/gemm-chaining-mesi.exe
+
+# DMA
+ENABLE_SM=1 COH_MODE=1 COMP_MODE=1 make gemm_stratus-app-clean gemm_stratus-app
+cp soft-build/ariane/sysroot/applications/test/gemm_stratus.exe test/gemm/gemm-chaining-dma.exe
 
--- socs/fcnn-spx-xilinx-vcu118-xcvu9p/test/run-gemm-test.sh
+++ socs/fcnn-spx-xilinx-vcu118-xcvu9p/test/run-gemm-test.sh
@@ -1,8 +1,11 @@
-# Spandex
-for length in 32 64 128 256 512 1024 # TODO
-do
-    echo ""
-    echo "GEMM: Spandex; Length=${length}" # TODO
-    echo ""
-    ./gemm-spx/gemm-spx-${length}.exe
-done
+# ASI SPX
+echo ""
+echo "GEMM: ASI SPX"
+echo ""
+./gemm/gemm-chaining-spx.exe 64 64 1
+./gemm/gemm-chaining-spx.exe 8
+./gemm/gemm-chaining-spx.exe 16
+./gemm/gemm-chaining-spx.exe 16 32 16
+./gemm/gemm-chaining-spx.exe 32
+./gemm/gemm-chaining-spx.exe 32 64 32
+./gemm/gemm-chaining-spx.exe 64
