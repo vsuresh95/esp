0302822c6df94ff45d59f454b31df5094c5d7d7b
vv15 <vv15@illinois.edu>
2025-06-02 12:53:54 -0500

    Fixed ubuntu22 buildroot issues

--- utils/toolchain/build_riscv_toolchain.sh
+++ utils/toolchain/build_riscv_toolchain.sh
@@ -12,7 +12,7 @@ LINUX_VERSION=4.20.0
 export SYSROOT=${ESP_ROOT}/soft/ariane/sysroot
 RISCV_GNU_TOOLCHAIN_SHA_DEFAULT=afcc8bc655d30cf6af054ac1d3f5f89d0627aa79
 RISCV_GNU_TOOLCHAIN_SHA_PYTHON=2c037e631e27bc01582476f5b3c5d5e9e51489b8
-BUILDROOT_SHA_DEFAULT=82e7e4d7874e3f1ba78d8cf53f5a34e8c2f0d09d
+BUILDROOT_SHA_DEFAULT=a4a13f23d3b24b5cd3203c9e216a6d0e70a47e91
 BUILDROOT_SHA_PYTHON=fbff7d7289cc95db991184f890f4ca1fcf8a101e
 
 # A patch for buildroot RISCV64 with numpy enabled
@@ -179,7 +179,7 @@ if [ $(noyes "Skip buildroot?") == "n" ]; then
     	git checkout .
     	git pull
     else
-        git clone https://github.com/vsuresh95/buildroot.git
+        git clone git@github.com:vsuresh95/buildroot.git
     	cd $src
     fi
 
